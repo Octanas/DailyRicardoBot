7bd93996b31881ba781a823190e910f8c9f984b2
Filipe Barbosa <up201909573@fe.up.pt>
2021-01-06 00:27:41 +0000

    Add basic bot

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ricardo') {
+        message.channel.send('<:pinto:795825395563364362>');
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
